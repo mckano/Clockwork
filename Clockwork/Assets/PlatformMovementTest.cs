cac11cf26a055d9bdd823a5a2f8109a6fa34eb8c
YesTea2 <85213325+YesTea2@users.noreply.github.com>
2021-06-22 19:33:36 -0700

    Added new script for platform movement test
    moving platforms now have a prefab for reference and a new script to control movement to take into consideration the players collision properly, time of platform adjusted to prevent stutters

---
+++ Clockwork/Assets/PlatformMovementTest.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovementTest : MonoBehaviour
+{
+    public Vector3 pointB;
+    public float speedP;
+    IEnumerator Start()
+    {
+       
+        var pointA = transform.position;
+        while (true)
+        {
+
+            yield return StartCoroutine(MoveObject(transform, pointA, pointB, speedP));
+            yield return StartCoroutine(MoveObject(transform, pointB, pointA, speedP));
+        }
+    }
+    // Start is called before the first frame update
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            speedP = 20.0f;
+        }
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            rb.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            rb.transform.parent = null;
+        }
+    }
+    IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+    {
+        var i = 0.0f;
+        var rate = 3.0f / time;
+        while (i < 1.0f)
+        {
+
+            i += Time.deltaTime * rate;
+            thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+            yield return null;
+        }
+    }
+      
+}
